061aee223a286a67f8f4f7d098c61e141f53f07c
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-15 19:14:34 +0300

    Funktion perusta ja docstring

--- prompt_auto.py
+++ prompt_auto.py
@@ -1,1 +1,9 @@
-
+def prompt_input(prompt, errormsg):
+    """
+Prompts the user for an integer using the prompt parameter.
+If an invalid input is given, an error message is shown using
+the error message parameter. A valid input is returned as an
+integer.
+"""
+    
+    return usr_input
