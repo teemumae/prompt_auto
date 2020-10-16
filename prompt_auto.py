ff78fde030b8286c9894168fc1e2f699150a3f93
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-16 17:56:14 +0300

    Input loopin muokkauksia

--- prompt_auto.py
+++ prompt_auto.py
@@ -5,11 +5,11 @@ If an invalid input is given, an error message is shown using
 the error message parameter. A valid input is returned as an
 integer.
 """
-    number = input(prompt)
+    number = int(input(prompt))
     while not isinstance(number, int):
         print(errormsg)
-        number = input(prompt)
-    return usr_input
+        number = int(input(prompt))
+    return number
 
 number = prompt_input(
     "Give an integer: ",
