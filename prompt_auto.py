dd06c616b89016f2457f4db8da704a53f81d45fa
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-16 17:51:05 +0300

    Input looppi

--- prompt_auto.py
+++ prompt_auto.py
@@ -5,7 +5,10 @@ If an invalid input is given, an error message is shown using
 the error message parameter. A valid input is returned as an
 integer.
 """
-    
+    number = input(prompt)
+    while not isinstance(number, int):
+        print(errormsg)
+        number = input(prompt)
     return usr_input
 
 number = prompt_input(
