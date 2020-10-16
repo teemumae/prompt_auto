1af13a6a7224b028a99b422ae53a77ff8719aa21
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-16 17:38:05 +0300

    Pääohjelma lisätty

--- prompt_auto.py
+++ prompt_auto.py
@@ -7,3 +7,14 @@ integer.
 """
     
     return usr_input
+
+number = prompt_input(
+    "Give an integer: ",
+    "You did not give an integer"
+)
+print("You gave the {} integer! Good job!".format(number))
+moogles = prompt_input(
+    "How many moogles are in the Moogle Village? ",
+    "This is not a valid number of moogles!"
+)
+print("There are {} moogles in the village.".format(moogles))
